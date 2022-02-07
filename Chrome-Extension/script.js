080dd7a8111e58601b947a12390ae3c32ebe37fb
Kyle S <kschumel@outlook.com>
2022-02-06 23:16:18 -0600

    Contiued work on Chrome Extension up to lesson 24

--- Chrome-Extension/script.js
+++ Chrome-Extension/script.js
@@ -1,5 +1,37 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+//cannot be reassigned. done because it gives a way to quickly signal what to do with variables 
+const inputEl = document.getElementById("input-el")
+//cannot be reassigned 
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
+//Push the value of inputBtn to myLeads when the input button is clicked
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked from addEventListener")
+    //I don't need document.getElementById("input-el").value becaue that is already grabbed on line 3. You just need the variable name.value  
+    myLeads.push(inputEl.value)
+    //This clears out the input value once it is rendered
+    inputEl.value = ""
+    renderLeads();
 })
+
+function renderLeads() {
+    //Create a variable, listItems, to hold all the HTML for the list items
+    let listItems = ""
+    // Log out the items in the myLeads array using a for loop 
+    for (let i = 0; i < myLeads.length; i++) {
+    //the += takes the previous value and concateates it with the next value
+    //the innerHTML converts the inside to <li>
+    //remember to put the html stuff in ""
+    //psudo-code // create element // set text content // append to ul
+    //Another way to do this is shown on line 45 of side-code
+    //in the href the wierd qoutes are there because you are closing off the html string with "" to add a JS variable. The first plus adds the var and the second plus starts the next string.
+    listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    console.log(listItems)
+    
+}
+    ulEl.innerHTML = listItems
+
+//.innerHTML inside or outside the loop? DOM manipulation comes with a cost. Is it better to change 3 time while in loop or once while outside? Think - is it easier to move 1 time or 3?
+}
+
+//on 24 template strings
--- Chrome-Extension/side-code/side-code.js
+++ Chrome-Extension/side-code/side-code.js
@@ -3,3 +3,58 @@ let box = document.getElementById("box")
 box.addEventListener("click", function(){
     console.log("I want to open the box!")
 })
+//gives a way to quickly signal what to do with variables 
+const playerName = "Per"
+let credits = 45
+
+credits = credits - 10
+
+// If possible, use const. If not, use let.
+
+// Which variables below should be changed from let to const?
+
+// The customer wants to order some stuff. Here are the details:
+const basePrice = 520
+const discount = 120
+let shippingCost = 12
+let shippingTime = "5-12 days"
+
+// Whops! Turns out the shipping will be a bit more complex
+shippingCost = 15
+shippingTime = "7-14 days"
+
+// Calculating the full price
+//since it's stated once it should also be const
+const fullPrice = basePrice - discount + shippingCost
+
+// Finally, notifying the customer
+console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
+
+// Use .innerHTML to render a Buy! button inside the div container
+
+const container = document.getElementById("container")
+
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+function buy() {
+    //forgot to add <p> element
+    container.innerHTML += "<p>Thank you for buying!</p>"
+}
+
+//the += takes the previous value and concateates it with the next value
+    //the innerHTML converts the inside to <li>
+    //remember to put the html stuff in ""
+    //psudo-code // create element // set text content // append to ul
+    //Another way to do this is shown on line 45 of side-code
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+const li = document.createElement("li")
+li.textContent = myLeads[i]
+ulEl.append(li)
+
+//declares and creates the list item
+const li = document.createElement("li")
+//sets the list item to the array for loop
+li.textContent = myLeads[i]
+//adds this new html element to the unordered list element
+ulEl.append(li)
